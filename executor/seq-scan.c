fed384577311e2e80e9b19ffe322141832208b0f
Jason Benn <jasoncbenn@gmail.com>
2017-02-15 03:17:07 -0800

    Simplify seqScan, get it compiling.

--- executor/seq-scan.c
+++ executor/seq-scan.c
@@ -5,31 +5,27 @@
 
 struct {
   Table *table;
-  int currentId; // Should be location of memory buffer.
+  int position;
   int numColumns;
-  char *columns[];
 } typedef SeqScanState;
 
-SeqScanState *seqScanInit(Table *table, char *columns[], int numColumns) {
+SeqScanState *seqScanInit(Table *table, int numColumns) {
   SeqScanState *state = (SeqScanState *) malloc(sizeof(SeqScanState));
   state->table = table;
-  state->currentId = 0;
-  state->numColumns = 10; // TODO: populate with data from a header in our binary file format?
-  *(state->columns) = *columns;
+  state->position = 0;
+  state->numColumns = 10; // TODO: populate with data from a header in our binary file format...?
   return state;
 }
 
 Record *seqScanNext(SeqScanState *state) {
-//  printf("%i\n", state->currentId);
-  //  printf("%s\n", *state->table[state->currentId]);
-  Record *record = &(state->table[sizeof(Record) * state->currentId]);
-  if (strcmp(*record, TABLE_TERMINATOR) == 0) {
-    return &TABLE_TERMINATOR;
+  // ?? Record *record = &(state->table[sizeof(Record) * state->position]);
+  Record *record;
+  if (state->position == state->numColumns) {
+    return NULL;
   } else {
-    state->currentId++;
+    state->position++;
     return record;
   }
-  return &TABLE_TERMINATOR;
 }
 
 void seqScanClose(SeqScanState *state) {
